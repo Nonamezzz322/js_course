a421fe6026922f723d6579a084ba0dc5c3938a54
Nonamezzz322 <nonamezzz322@gmail.com>
2018-09-18 19:10:41 +0300

    add lesson_9

---
+++ lesson_9/index.js
@@ -0,0 +1,32 @@
+'use strict'
+
+console.log("Вовчок Анатолий");
+let a = 13;
+let b = 2;
+
+if (a > b) {
+    console.log(a);
+} 
+    else {
+        console.log(b);
+}
+
+if (a > b) {
+    console.log(b);
+} 
+    else {
+        console.log(a);
+}
+
+console.log(a + b);
+console.log(a % b);
+
+if (a > b + 10) {
+    console.log(a + 10);
+} 
+    else if (a < b) {
+        console.log(9);
+    }
+        else {
+            console.log(b);
+        }
