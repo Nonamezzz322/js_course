7adeea09d8d67af2141602fe459b3a008a28d509
Nonamezzz322 <nonamezzz322@gmail.com>
2018-09-24 20:54:53 +0300

    lesson 11_3

--- lesson_11/index.js
+++ lesson_11/index.js
@@ -30,14 +30,19 @@ function absDiff(a) {
     }
 }
 
-// function RemoveKeys2(obj, arr) {
+//второй, возможно правильный вариант решения
+// function removeKeys2(obj, arr) {
 //     let newObj = {};
-//     for (let objElem of (Object.keys(obj))){
-//         for (let arrElem of arr){
-//             if (objElem !== arrElem){
-//                 newObj[objElem] = obj[objElem];
+//     for (let objElem of Object.keys(obj)) {
+//         for (let arrElem of arr) {
+//             if (objElem === arrElem) {
+//                 delete obj[objElem];
 //             }
 //         }
+//     }
+//     let keys = Object.keys(obj);
+//     for (let element of keys){
+//         newObj[element] = obj[element];
 //     }
 //     return newObj;
 // }
