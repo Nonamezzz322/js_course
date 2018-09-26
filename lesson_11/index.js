fa546e60f831878151657bc16680634909c2b96e
Nonamezzz322 <nonamezzz322@gmail.com>
2018-09-26 19:06:50 +0300

    lesson 11_4

--- lesson_11/index.js
+++ lesson_11/index.js
@@ -1,28 +1,36 @@
 'use strict'
 
-function RemoveKeys1(obj, arr){
-    for (let objElem of (Object.keys(obj))){
-        for (let arrElem of arr){
-            if (objElem === arrElem){
-                delete obj[objElem];
-            }
-        }
-    }
-    return obj;
+function removeKeys1(object, keys) {
+  for (let i = 0; i < keys.length; i += 1) {
+    delete object[keys[i]];
+  }
+
+  return object;
 }
 
-function RemoveKeys2(obj, arr) {
+function removeKeys2(object, omit) {
+  const result = {};
+  const keys = Object.keys(object);
 
-  let newObj = obj;
-    for (let objElem of (Object.keys(obj))) {
-        for (let arrElem of arr) {
-            if (objElem === arrElem) {
-                delete newObj[objElem];
-            }
-        }
+  for (let i = 0; i < keys.length; i += 1) {
+    const key = keys[i];
+    let exist = false;
+
+    for (let j = 0; j < omit.length; j += 1) {
+      if (key === omit[j]) {
+        exist = true;
+        break;
+      }
     }
-    return newObj;
+
+    if (!exist) {
+      result[key] = object[key];
+    }
+  }
+
+  return result;
 }
+
 
 function absDiff(a) {
     return function calculate(b) {
