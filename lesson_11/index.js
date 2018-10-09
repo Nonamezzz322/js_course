d6d29ac2146965d3cf1e3509d7f18a69fbd07fa7
Nonamezzz322 <nonamezzz322@gmail.com>
2018-10-09 17:15:00 +0300

    1

--- lesson_11/index.js
+++ lesson_11/index.js
@@ -1,34 +1,21 @@
 'use strict'
 
-function removeKeys1(object, keys) {
-  for (let i = 0; i < keys.length; i += 1) {
-    delete object[keys[i]];
-  }
-
-  return object;
+function removeKeys1(obj, arr) {
+  for( let i = 0; i < arr.length; i += +1) {
+    delete obj[arr[i]]
 }
+  return obj;
+};
 
-function removeKeys2(object, omit) {
-  const result = {};
-  const keys = Object.keys(object);
+function removeKeys2(obj, arr) {
+  let newObj = {};
 
-  for (let i = 0; i < keys.length; i += 1) {
-    const key = keys[i];
-    let exist = false;
-
-    for (let j = 0; j < omit.length; j += 1) {
-      if (key === omit[j]) {
-        exist = true;
-        break;
+  for (let key in obj) {
+      if (arr.indexOf(key) === -1) {
+          newObj[key] = obj[key];
       }
-    }
-
-    if (!exist) {
-      result[key] = object[key];
-    }
   }
-
-  return result;
+  return newObj;
 }
 
 
@@ -37,20 +24,3 @@ function absDiff(a) {
         return Math.abs(a - b);
     }
 }
-
-//второй, возможно правильный вариант решения
-// function removeKeys2(obj, arr) {
-//     let newObj = {};
-//     for (let objElem of Object.keys(obj)) {
-//         for (let arrElem of arr) {
-//             if (objElem === arrElem) {
-//                 delete obj[objElem];
-//             }
-//         }
-//     }
-//     let keys = Object.keys(obj);
-//     for (let element of keys){
-//         newObj[element] = obj[element];
-//     }
-//     return newObj;
-// }
